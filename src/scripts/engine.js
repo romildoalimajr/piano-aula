133212c41039365e91ff0f0af390d22713dcf922
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-03 22:16:35 -0300

    Criando o áudios das teclas.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -11,4 +11,10 @@ const playTune = (key) => {
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
-})
+});
+
+document.addEventListener("keydown",
+    (e) => {
+        playTune(e.key);
+        console.log(e.key);
+    });
